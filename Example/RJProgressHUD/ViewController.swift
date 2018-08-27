16fe822c8ad07b836de149297d4cafac7acb54dd
RJ <841348093@qq.com>
2018-08-27 16:47:17 +0800

    0.1.2

--- Example/RJProgressHUD/ViewController.swift
+++ Example/RJProgressHUD/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
-
+import RJProgressHUD
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        RJProgressHUD.showMessage("hahahhah", in: view)
     }
 
     override func didReceiveMemoryWarning() {
